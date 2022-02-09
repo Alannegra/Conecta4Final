c235be1a81afb1584d57df1374e8ae2f739d2cce
Alannegra <alanrodriguez@elpuig.xeill.net>
2022-02-09 17:58:19 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escribe tu nombre:");
+        String jugador1 = scanner.nextLine();
+
+	// write your code here
+    }
+}
